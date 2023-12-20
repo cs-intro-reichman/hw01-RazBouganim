73eea24d61dab65b356a1dd0d688535e8b21cfb2
Raz Bouganim <raz.bouganim@post.runi.ac.il>
2023-12-20 15:49:59 +0200

    Raz's Homework 01

---
+++ AddTwo.java
@@ -0,0 +1,7 @@
+public class AddTwo {
+	public static void main(String[] args) {
+		int i = Integer.parseInt(args[0]);
+		int j = Integer.parseInt(args[1]);
+		System.out.println(i + " + " + j + " = " + (i+j));
+		}
+}
